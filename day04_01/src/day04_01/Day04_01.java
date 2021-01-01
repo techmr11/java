648d592a29aafcf88ba80b19f210086d69958dd5
techm <techm@techmr_Gram17>
2021-01-02 04:08:46 +0900

    test

---
+++ day04_01/src/day04_01/Day04_01.java
@@ -0,0 +1,8 @@
+package day04_01;
+public class Day04_01 {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요!");
+		System.out.println("반갑습니다!!");
+		System.out.println("print 와 println의 차이점입니다");
+	}
+}
